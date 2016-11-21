9cf02a13da4504b377c1170f0742be4d90a79cd4
TFKamper <tfkamper@gmail.com>
2016-11-21 19:44:19 +0100

    Add files via upload

---
+++ html5passion/js/script.js
@@ -0,0 +1,8 @@
+/** External Links **/
+
+function externalLinks() { if (!document.getElementsByTagName) return; var anchors = document.getElementsByTagName("a"); for (var i=0; i<anchors.length; i++) { var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank"; } } window.onload = externalLinks;
+
+
+/** Gallery **/
+
+$(document).ready(function(){	$("#gallerycont").easySlider({ controlsBefore:	'<p id="controls">', controlsAfter:	'</p>', auto: true, continuous: true }); });
